a16a4978570ebc871b6b1d17ed958337ff16adb6
Wired Earp <jesper.moth@lego.com>
2022-10-26 07:13:05 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+// `app/page.js` maps to root `/` URL
+export default function Page() {
+  return <h1>Hello from root</h1>;
+}
---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  basePath: '/myapp',
+  experimental: { appDir: true },
+}
